5d1eeb36f75b73516c4f5aa91921bdbbe417d13d
Anshuman Gupta <anshuman844@gmail.com>
2020-01-12 14:14:31 +0530

    Markdown Previewer version 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
 import './App.scss';
+import Toolbar from './toolbar';
+import Editor from './editor';
+import Previewer from './previewer';
 
 class TextEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+    this.getTool = this.getTool.bind(this);
+    this.handleContent = this.handleContent.bind(this);
+  }
+
+  getTool = (tool) => {
+    console.log(tool);
+  }
+
+  handleContent = (content) => {
+    this.setState({
+      content: content
+    });
+  }
+
   render() {
     return (
-      <main></main>
+      <main className="container">
+        <Editor handleContent={this.handleContent}>
+          <Toolbar handleTool={this.getTool} />
+        </Editor>
+        <Previewer content={this.state.content}/>
+      </main>
     );
   }
 }
